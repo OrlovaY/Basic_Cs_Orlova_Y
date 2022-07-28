9bb78316d01e891ac64387b25c29c62a8743f33d
yuliya.orlova <orel.y.v@mail.ru>
2022-07-29 01:17:29 +0300

    added 005 task

---
+++ 005/Program.cs
@@ -0,0 +1,18 @@
+﻿//С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+int a=-110, b=47, c=5;
+int max;
+if (a>b && a>c)
+{
+max=a;
+System.Console.WriteLine(max);
+}
+if (b>a && b>c)
+{
+max=b;
+System.Console.WriteLine(max);
+}
+if (c>a && c>b)
+{
+    max=c;
+    System.Console.WriteLine(max);
+}
