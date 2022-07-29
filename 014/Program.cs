41b80e445f5cb3c53037b9c80c7611df3f35683e
yuliya.orlova <orel.y.v@mail.ru>
2022-07-29 03:41:43 +0300

    added 014 task

---
+++ 014/Program.cs
@@ -0,0 +1,18 @@
+﻿//С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b, 
+//если нет, вывести остаток от деления a на b.
+
+int a=23;
+int b=5;
+int x =a%b;
+if (x==0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine("Некратно");
+}
+if (x>0)
+{
+    System.Console.WriteLine(x);
+}
