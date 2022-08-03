7c7c1a49182bce8918a3589331dabd134187a4df
yuliya.orlova <orel.y.v@mail.ru>
2022-08-03 14:58:26 +0300

    added changes to task 028

---
+++ 028/Program.cs
@@ -0,0 +1,24 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+
+System.Console.WriteLine(CountDigits(1245));
+
+
+
+
+
+
+int CountDigits(int N)
+
+{
+    int k=0;
+    while (N!=0)
+    {
+        //k=k+1;
+        k++;
+        //N=N/10;
+        N/=10;
+    }
+    return k;
+}
+
+
