635f2d3b7fea6338643c0c348a3646f3e6bf29d7
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 17:22:50 +0300

    added task 027 finale

--- 027/Program.cs
+++ 027/Program.cs
@@ -1,6 +1,16 @@
 ﻿// Возведите число А в натуральную степень B используя цикл
-
+/*
 int A=1;
 int B=3;
 double pow=Math.Pow(A,B);
 System.Console.WriteLine(pow);
+*/
+
+int A=3;
+int B=3;
+int b = 1;
+for(int i=1;i<=B;i++) 
+{
+   b *= A; //b=b*A; 
+}
+System.Console.WriteLine(b);
