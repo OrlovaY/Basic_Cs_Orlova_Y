a7b083f4a2eb1e30bcfa9cffdfd4c29c59103764
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 14:20:53 +0300

    added task 024

---
+++ 024/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+int a=1;
+int N=10;
+int y=a*a;
+while (a<=N)
+    {
+        y=a*a;
+        a=a+1;
+        System.Console.Write($"{y,4}");
+    }
+
