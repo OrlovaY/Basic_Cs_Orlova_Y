7328eb0430f6ec7ad5de7d2eeb7c75f6664ff8f0
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 12:48:32 +0300

    added task 038 finale

--- 038/Program.cs
+++ 038/Program.cs
@@ -7,6 +7,13 @@ Random random=new Random();
 for(int i=0;i<a.Length;i++)      
     a[i]=random.Next(0,10);     
 for(int i=0;i<a.Length;i++)
-    System.Console.Write($"{a[i],4}"); 
+ System.Console.Write($"{a[i],4}"); 
+
+int count=0;
+for(var i=0; i<a.Length;i++)
+{
+   if(i>10 && i<99)
+     count++;
+}
 System.Console.WriteLine();
-   System.Console.WriteLine(a.Length);
+System.Console.WriteLine(count);
