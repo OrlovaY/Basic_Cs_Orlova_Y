b5bbd3ee93e45ea40c0542ab45ca011724119912
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 16:01:08 +0300

    added task 029

---
+++ 029/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+
+int a = 1111; //int a=int.Parse(Console.ReadLine()); для ввода числа с клавиатуры
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
