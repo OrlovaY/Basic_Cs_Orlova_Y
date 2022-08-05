a13c6ba817231c91fe50f8ca1ba1f574787ef7ce
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 17:08:17 +0300

    added task 031

---
+++ 031/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+int[] array = new int[] {12,4,45,34,73,12};
+int i=0;
+double pow=Math.Pow(array[i],3);
+for(i=0;i<array.Length;i++)
+
+    if((array[i] % 2) == 0) 
+    {
+    pow=Math.Pow(array[i],3);
+    System.Console.WriteLine($"{pow}");
+    }
+
+
+else
+{
+    System.Console.WriteLine("Число заканчивается на нечетную цифру");
+}
