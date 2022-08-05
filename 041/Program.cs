34dd1b6bef0b49e625e9d3ab31ba7835a517e03a
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 23:10:59 +0300

    added task 041 start

--- 041/Program.cs
+++ 041/Program.cs
@@ -1,3 +1,18 @@
 ﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным 
 //и минимальным элементом
+/*
+int N=5;
+double [] a = new double[N];
+Random rand = new Random();
+for (int i = 0; i < a.Length; i++)
+a[i] = rand.Next(-100, 100) + rand.NextDouble();
+System.Console.WriteLine(a);
+*/
 
+int N=8;
+double[] a=new double[N];              //объявить массив, задать его размер
+Random random=new Random();
+for(int i=0;i<a.Length;i++)      //цикл for
+    a[i]=random.Next(0,100);     //генерирует случайные числа
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}"); 
