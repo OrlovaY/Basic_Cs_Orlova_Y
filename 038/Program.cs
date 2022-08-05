af0fe8264412675748cb92047a11afed216f3c19
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 04:09:01 +0300

    added task 038 start

---
+++ 038/Program.cs
@@ -0,0 +1,12 @@
+﻿// В одномерном массиве из 123 чисел 
+//найти количество элементов из отрезка [10,99]
+
+int N=123;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)      
+    a[i]=random.Next(0,10);     
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}"); 
+System.Console.WriteLine();
+   System.Console.WriteLine(a.Length);
