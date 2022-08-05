05473d7ab102e86017032e80dce50bcd47dc6dcf
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 12:10:26 +0300

    added task 035 start

---
+++ 035/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу замены элементов массива на противоположные
+
+int N=10;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)      
+    a[i]=random.Next(-10,10);     
+for(int i=0;i<a.Length;i++)
+
+
+for(i=0; i<a.Length; i++)
+    a[i]=a[i]*-1;
+    System.Console.Write($"{a[i],4}");
+System.Console.WriteLine();
