c58b5a37add57c2fcaaa2fef7e0fab8cebe459fc
yuliya.orlova <orel.y.v@mail.ru>
2022-08-05 15:01:44 +0300

    added task 025

---
+++ 025/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести на экран кубы чисел от 1 до N
+
+int a=1;
+int N=5;
+double pow = Math.Pow(a,3);
+while (a<=N)
+    {
+        pow = Math.Pow(a,3);
+        a++;
+        System.Console.WriteLine($"{pow}");
+    }
