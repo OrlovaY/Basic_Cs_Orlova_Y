83cdaabc1dc6a6d79308d9344927a1a9852ace5e
yuliya.orlova <orel.y.v@mail.ru>
2022-08-09 13:16:04 +0300

    added task 021

---
+++ 021/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+int qNum;
+qNum=Convert.ToInt32(Console.ReadLine());
+if (qNum==1)
+{
+    System.Console.WriteLine("x>0; y>0");
+}
+else if (qNum==2)
+{
+    System.Console.WriteLine("x<0; y>0");
+}
+else if (qNum==3)
+{
+    System.Console.WriteLine("x<0; y<0");
+}
+else if (qNum==4)
+{
+    System.Console.WriteLine("x>0; y<0");
+}
+else if (qNum>4)
+{
+    System.Console.WriteLine("Ошибка!");
+}
