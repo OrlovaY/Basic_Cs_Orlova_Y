fa495399ef8474af81b1792c2caf48c57d3c864f
yuliya.orlova <orel.y.v@mail.ru>
2022-08-09 15:53:22 +0300

    added task 043

---
+++ 043/Program.cs
@@ -0,0 +1,19 @@
+﻿// С клавиатуры вводится число N. Затем вводятся N чисел. 
+//Определить сколько чисел больше 0 введено с клавиатуры
+
+//int N=int.Parse(Console.ReadLine());
+using System;
+Console.ReadLine();
+string s=Console.ReadLine();
+string[] ar=s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+int[] a=Array.ConvertAll<string,int>(ar,int.Parse);
+int countPositive=0;
+for (int i=0;i<ar.Length;i++)
+{
+    if (int.Parse(ar[i])>0)
+    {
+    countPositive++;
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine(countPositive);
