94f12e6bea72d5fe8d0909b0a22a125a7325330b
yuliya.orlova <orel.y.v@mail.ru>
2022-08-09 16:32:00 +0300

    added task 048

---
+++ 048/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу копирования массива
+
+using System;
+/*
+int[] arr = { 5, 4, 7, 2, 9 };
+ 
+        int[] copy = new int[arr.Length];
+        arr.CopyTo(copy, 0);
+
+Console.WriteLine(String.Join(", ", copy));  
+*/
+
+int[] arr = { 5, 4, 7, 2, 9 };
+ 
+        int[] copy = new int[arr.Length];
+        for (int i = 0; i < arr.Length; i++) 
+        {
+            copy[i] = arr[i];
+        }
+ 
+        Console.WriteLine(String.Join(", ",copy)); 
