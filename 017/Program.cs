7ac5b3923baafa49a5b6df119b2dfef6dee0ba6a
yuliya.orlova <orel.y.v@mail.ru>
2022-08-09 12:52:12 +0300

    added task 017 finale

---
+++ 017/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число обозначающее день недели. 
+//Выяснить является номер дня недели выходным
+
+int dayOfWeek;
+dayOfWeek=Convert.ToInt32(Console.ReadLine());
+if (dayOfWeek==6 || dayOfWeek==7)
+{
+    System.Console.WriteLine("Выходной");
+}
+else if (dayOfWeek>=1 && dayOfWeek<=5)
+{
+    System.Console.WriteLine("Рабочий день");
+}
+else
+{
+    System.Console.WriteLine("Ошибка! День не найден!");
+}
+
+
