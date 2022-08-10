e113d22a0f555e326bb7730b4fde7da101371be5
yuliya.orlova <orel.y.v@mail.ru>
2022-08-10 21:11:34 +0300

    added task 056_2

--- 056/Program.cs
+++ 056/Program.cs
@@ -15,15 +15,41 @@ int[,] Create2DArray(int m,int n)
     return a;
 }
 Print(arr);
-System.Console.WriteLine();
+System.Console.WriteLine(a.GetLength(0),0);//?
+Swap(arr);
 
 void Print(int[,] a)
 {
-    for(int i=0;i<a.GetLength(0);i++)
+    for(int i=0;i<a.GetLength(0);i++)// строка
     {
-            for(int j=0;j<a.GetLength(1);j++)
+            for(int j=0;j<a.GetLength(1);j++)//столбец
                 System.Console.Write($"{a[i,j],4}");
             System.Console.WriteLine();
     }
 }
-void Swap()
+
+
+void Swap(int[,] a)
+{
+    
+}
+
+
+
+/*
+for(int i=0;i<a.GetLength(0)%2;i++)
+    {
+        a.GetLength(0) a.GetLength(0)-1
+
+     }
+     */
+
+//ref int b, ref int c
+/*
+for(int j=0;j<a.GetLength(1);j++)
+    {
+        int c = a [0,0];
+        int b = a [a.GetLength(1)-1,0];
+        int t = c;
+    }
+    */
