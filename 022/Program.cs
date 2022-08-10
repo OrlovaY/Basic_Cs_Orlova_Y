b8e392784cace029540f5a3ce2c0d35847c2878a
yuliya.orlova <orel.y.v@mail.ru>
2022-08-10 19:08:31 +0300

    added task 022

--- 022/Program.cs
+++ 022/Program.cs
@@ -1,17 +1,31 @@
 ﻿// Программа проверяет пятизначное число на палиндром
-/*
+
 Console.WriteLine("Введите пятизначное число");
-            string x = Console.ReadLine();
-            char[] arr = x.ToCharArray();
-            Array.Reverse(arr);
-            string x2 = new String(arr);
-            if (x == x2) 
-            {
-            Console.WriteLine("Палиндром");
-            }
-            else 
-            {
-            Console.WriteLine("Не палиндром");
-            }
-*/
+string? x = Console.ReadLine();
+char[] arr = x.ToCharArray();
+Array.Reverse(arr);
+string x2 = new String(arr);
+if (x == x2) 
+    {
+        Console.WriteLine("Палиндром");
+    }
+else 
+    {
+        Console.WriteLine("Не палиндром");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
