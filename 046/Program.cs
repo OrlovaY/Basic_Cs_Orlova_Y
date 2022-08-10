68402ce4a6d4d8a0346c2129ef69e7abefb56d47
yuliya.orlova <orel.y.v@mail.ru>
2022-08-10 19:30:01 +0300

    added task 046 finale

--- 046/Program.cs
+++ 046/Program.cs
@@ -1,6 +1,29 @@
 ﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. 
 //Принять первые числа равными 0 и 1
 
+using System;
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] a=new int[N];
+int fib0=0;
+System.Console.WriteLine(fib0);
+int fib1=1;
+System.Console.WriteLine(fib1);
+a[0]=fib0;
+a[1]=fib1;
+a[2]=fib0+fib1;
+for (int i=2;i<a.Length;i++)
+{
+    a[i]=a[i-1]+a[i-2];
+    System.Console.WriteLine(a[i]);
+}
+
+
+
+
+
+
+
 /*
 using System;
 Console.ReadLine();
@@ -8,16 +31,3 @@ string s=Console.ReadLine();
 string[] ss=s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
 int[] a=Array.ConvertAll<string,int>(ss,int.Parse);
 */
-using System;
-int N = Int32.Parse(Console.ReadLine());
-int[] a=new int[N];
-int fib1=0;
-int fib2=1;
-a[0]=fib1;
-a[1]=fib2;
-a[3]=fib1+fib2;
-for (int i=3;i<a.Length;i++)
-{
-    a[i]=a[i-1]+a[i-2];
-}
-System.Console.WriteLine(a);
