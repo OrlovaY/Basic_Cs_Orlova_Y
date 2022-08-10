69e75fed41b3d9c7a4011c524eec39a7bafccdb2
yuliya.orlova <orel.y.v@mail.ru>
2022-08-10 18:12:56 +0300

    added task 056 start

---
+++ 056/Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] arr;
+int m =Convert.ToInt32(Console.ReadLine());
+int n =Convert.ToInt32(Console.ReadLine());
+arr=Create2DArray(m,n);
+
+int[,] Create2DArray(int m,int n)
+{
+    int[,] a=new int[m,n];
+    Random random=new Random();
+    for(int i=0;i<a.GetLength(0);i++)
+        for(int j=0;j<a.GetLength(1);j++)
+            a[i,j]=random.Next(0,100);
+    return a;
+}
+Print(arr);
+System.Console.WriteLine();
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+            for(int j=0;j<a.GetLength(1);j++)
+                System.Console.Write($"{a[i,j],4}");
+            System.Console.WriteLine();
+    }
+}
+void Swap()
