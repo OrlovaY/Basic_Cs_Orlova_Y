55cf7fcbc531cffea96b00e4ce3120905df2647d
yuliya.orlova <orel.y.v@mail.ru>
2022-08-17 23:17:06 +0300

    added change to task 071_02

--- 071/Program.cs
+++ 071/Program.cs
@@ -4,8 +4,8 @@ Console.Write("Введите число n: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число m: ");
 int n = Convert.ToInt32(Console.ReadLine());
-A(m, n);
-            
+
+
 int A(int m, int n)
     {
         if (m == 0) return n + 1;
