834809143d22d769d99bedcbc5c664d2ffcfed70
yuliya.orlova <orel.y.v@mail.ru>
2022-08-21 17:47:43 +0300

    added folder string 5

---
+++ string 5/Program.cs
@@ -0,0 +1,30 @@
+﻿// string5. Определить являются ли введенные с клавиатуры символы 
+//правильно записью целого числа. 
+//Вычислить сумму цифр введенного числа
+
+using System;
+ 
+namespace Sem8
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string? s = Console.ReadLine();
+            int temper = 0;
+                
+                if (!Int32.TryParse(s, out temper))
+                    
+                    {
+                        Console.WriteLine("Это не является верной записью целого");
+                    }
+                else 
+                    {
+                        int sum = 0;
+                        foreach (char digit in s)
+                        sum += digit - 48;
+                Console.WriteLine(sum);
+                    }
+        }
+    }
+}
