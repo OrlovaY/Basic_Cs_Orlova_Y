de862262c1eeb4f1afee01d8e0e2a1ab9e965871
yuliya.orlova <orel.y.v@mail.ru>
2022-08-21 16:48:31 +0300

    added commit sem9_2

---
+++ string3/Program.cs
@@ -0,0 +1,8 @@
+﻿// С клавиатуры вводится строка разделенная точкой. 
+//Подсчитать количество символов до точки
+
+using System.Linq; 
+string data = Console.ReadLine(); 
+int count = data.Split(".").First().Length;
+Console.WriteLine(count);
+
